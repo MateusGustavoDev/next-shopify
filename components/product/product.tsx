d48bba2d056d96bfbff77a1264d10b7c556f9187
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-11-17 16:09:57 -0300

    feat: product condition metafield

--- components/product/product.tsx
+++ components/product/product.tsx
@@ -19,6 +19,7 @@ export async function Product({ handle }: ProductProps) {
   const variants = removeEdgesAndNodes(product.variants)
   const images = removeEdgesAndNodes(product.images)
   const options = product.options
+  const productCondition = product.metafields.find((metafild) => metafild?.key === 'condition')
 
   return (
     <div className="tablet:flex-col relative flex h-max w-full gap-5">
@@ -26,7 +27,10 @@ export async function Product({ handle }: ProductProps) {
       <div className="tablet:aspect-auto tablet:max-w-full tablet:gap-10 flex w-full max-w-[440px] flex-col justify-between rounded-xl border border-neutral-800 bg-neutral-900 p-6">
         <div>
           <div className="flex flex-col gap-3">
-            <span className="block text-sm font-normal text-blue-500">Produto novo | Disponível</span>
+            <span className="block text-sm font-normal text-blue-500">
+              {productCondition ? `${productCondition.value} |` : null}{' '}
+              {product.availableForSale ? 'Disponível' : 'Indisponível'}
+            </span>
             <span className="tablet:text-xl text-3xl font-bold">{product.title}</span>
             <Price amount={price} variants={variants} options={options} />
           </div>
--- lib/shopify/graphql/fragments/product.ts
+++ lib/shopify/graphql/fragments/product.ts
@@ -43,6 +43,11 @@ export const productFragment = /* GraphQL */ `
         }
       }
     }
+    metafields(identifiers: [{ namespace: "product", key: "condition" }]) {
+      id
+      key
+      value
+    }
     tags
     updatedAt
   }

f17d1a905dafb5fb5ae9562459e8ddd5e72361b6
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-11-23 19:05:09 -0300

    reverse the order of carousel

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -24,14 +24,14 @@ export default function HomePage() {
       <div className="mt-10">
         <div className="flex w-full flex-col gap-8">
           <Suspense fallback={<CarouselSkeleton />}>
-            <ProductsCarousel collection="mais-vendidos" />
+            <ProductsCarousel collection="smartphones" />
           </Suspense>
           <div className="relative aspect-[1400/400] w-full overflow-hidden rounded-md border-neutral-700 lg:mt-0 lg:aspect-[992/400]">
             <Image fill src={banner_fones} alt="" className="lg:hidden" />
             <Image fill src={banner_fones_mobile} alt="" className="hidden lg:block" />
           </div>
           <Suspense fallback={<CarouselSkeleton />}>
-            <ProductsCarousel collection="smartphones" />
+            <ProductsCarousel collection="mais-vendidos" />
           </Suspense>
         </div>
       </div>
